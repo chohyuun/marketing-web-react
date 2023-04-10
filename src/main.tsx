0f7722659f9ffb819be81794de3602b8293f7254
조현지 <qndjddl001@gmail.com>
2023-04-10 20:42:32 +0900

    fix: style, route 처리 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import {StrictMode} from 'react'
-import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const root = createRoot(document.getElementById('root') as HTMLElement)
+
+root.render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
--- src/style/DefaultLayout.tsx
+++ src/style/DefaultLayout.tsx
@@ -1,11 +1,41 @@
-import { Container } from 'react-bootstrap'
+import styled from 'styled-components'
+import { Outlet } from 'react-router-dom'
 
 const DefaultLayout = () => {
   return (
     <>
-      <Container fluid></Container>
+      <Layout>
+        <Header />
+        <Content>
+          <Outlet />
+        </Content>
+      </Layout>
     </>
   )
 }
 
 export default DefaultLayout
+
+const Layout = styled.div`
+  width: 100vw;
+  height: 100vh;
+  color: #000000;
+  background: #ffffff;
+`
+
+const Header = styled.header`
+  z-index: 999;
+  display: flex;
+  width: 100%;
+  height: 45px;
+  top: 0;
+  left: 0;
+  right: 0;
+  background-color: #213547;
+`
+
+const Content = styled.body`
+  position: relative;
+  width: 100vw;
+  flex-direction: column;
+`
