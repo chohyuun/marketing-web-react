b16ab987ae1cd46af331cdea467f2d0027de2d1d
조현지 <qndjddl001@gmail.com>
2023-04-10 16:40:58 +0900

    feat: 프로젝트 기본 세팅 완료

---
+++ src/style/DefaultLayout.tsx
@@ -0,0 +1,11 @@
+import { Container } from 'react-bootstrap'
+
+const DefaultLayout = () => {
+  return (
+    <>
+      <Container fluid></Container>
+    </>
+  )
+}
+
+export default DefaultLayout
