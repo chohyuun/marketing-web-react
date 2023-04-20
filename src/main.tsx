8c619767f1bc489a628d20a8131dce56c4d3d0d6
조현지 <qndjddl001@gmail.com>
2023-04-20 14:39:24 +0900

    fix: react-bootstrap error 처리
    style 적용 안되고 있어 수정 작업
    - main.tsx 에서 style import 진행

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import { GlobalStyle } from './style/global'
 import { createGlobalStyle } from 'styled-components'
 import reset from 'styled-reset'
+import 'bootstrap/scss/bootstrap.scss'
 
 const root = createRoot(document.getElementById('root') as HTMLElement)
 
